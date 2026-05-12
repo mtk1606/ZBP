07534a330c377bc58b4d19ce5351d0001d9a83f3
Mohamed El khoudimi <mokhoudimi@gmail.com>
2026-05-11 20:29:21 -0400

    Add multiple picture imports to index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,9 @@
 import bg from './bg.jpg'
 
-import picture from './picture.png'
+import picture1 from './picture1.png'
+import picture2 from './picture2.png'
+import picture3 from './picture3.png'
+import picture4 from './picture4.png'
 
 import confetti from './confetti.gif'
 
@@ -10,7 +13,10 @@ import CakeSVG from './svg/CakeSVG'
 
 export {
     bg,
-    picture,
+    picture1,
+    picture2,
+    picture3,
+    picture4,
     confetti,
     CakeSVG
 }
